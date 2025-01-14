cefe80efb9cb6104c45a5f601f0376f2acc14daa
Mohamed Amine DAHMANI <ma.dahmani@autobiz.com>
2025-01-14 12:07:00 +0100

    basic file conversion

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FfmpegController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::post('/convert', [FfmpegController::class, 'convert']);
